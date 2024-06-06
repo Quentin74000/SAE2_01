2ff46a04b3764d5776b666cfda4c9f532c56dae1
Kenny Dodey <kenny.dodey@etu.univ-smb.fr>
2024-06-06 09:25:39 +0200

    indication de la page actuelle

--- SAE2_01/MainWindow.xaml.cs
+++ SAE2_01/MainWindow.xaml.cs
@@ -37,5 +37,33 @@ namespace SAE2_01
             InitializeComponent();
 
         }
+        private void But_inscrire_Click(object sender, RoutedEventArgs e)
+        {
+            tcGlobal.SelectedItem=Tab_Incription;
+        }
+
+        private void tcGlobal_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (tcGlobal.SelectedItem == Tab_Accueil)
+            {
+                Lab_Nom_Page.Content = string.Empty;
+                Lab_Nom_Page.Content = "Accueil";
+            }
+            else if (tcGlobal.SelectedItem == Tab_Incription)
+            {
+                Lab_Nom_Page.Content = string.Empty;
+                Lab_Nom_Page.Content = "Inscription";
+            }
+            else if (tcGlobal.SelectedItem == Tab_Visualiser_Coureur)
+            {
+                Lab_Nom_Page.Content = string.Empty;
+                Lab_Nom_Page.Content = "Rechercher un coureur";
+            }
+            else
+            {
+                Lab_Nom_Page.Content = string.Empty;
+                Lab_Nom_Page.Content = "Rechercher une course";
+            }
+        }
     }
 }
