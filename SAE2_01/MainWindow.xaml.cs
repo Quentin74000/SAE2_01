53314ae79c30cbd735a7688211b8d210fd534a21
Kenny Dodey <kenny.dodey@etu.univ-smb.fr>
2024-06-06 09:03:31 +0200

    connexion a la bd avec les différents comptes

--- SAE2_01/MainWindow.xaml.cs
+++ SAE2_01/MainWindow.xaml.cs
@@ -24,12 +24,16 @@ namespace SAE2_01
         {
             bool resultat;
             SeConnecter seConnecter = new SeConnecter();
-            resultat = (bool)seConnecter.ShowDialog();
-            if (resultat == true)
+            ConnexionBD dataconnexion = new ConnexionBD();
+            do
             {
-                ApplicationData dataconnexion = new ApplicationData();
-                dataconnexion.ConnexionBD();
-            }
+                seConnecter = new SeConnecter();
+                resultat = (bool)seConnecter.ShowDialog();
+                if (resultat == true)
+                {
+                    dataconnexion = new ConnexionBD();
+                }
+            } while (dataconnexion.Connexion() == false);
             InitializeComponent();
 
         }
